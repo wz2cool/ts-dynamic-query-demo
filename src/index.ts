a6b3f93336fd8e56e6635262e27eb58fc17374e0
wz2cool <wz2cool@live.cn>
2018-08-12 10:35:21 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { values } from "./data";
 import { User } from "./models/user";
+import { DynamicQuery, FilterOperator, QueryProvider } from "ts-dynamic-query";
 
 const dataStr = JSON.stringify(values);
 const users = User.getUsersFromJSON(dataStr);
 
-console.log(users.length);
+filterUserIdGreaterThan500();
 
-function filterStudent() {}
+function filterUserIdGreaterThan500() {
+  console.log("total users count: ", users.length);
+  const query = new DynamicQuery<User>().addFilter({
+    propertyPath: "id",
+    operator: FilterOperator.GREATER_THAN,
+    value: 500
+  });
+
+  const result = QueryProvider.query(users, query);
+  console.log("after filter user count: ", result.length);
+}

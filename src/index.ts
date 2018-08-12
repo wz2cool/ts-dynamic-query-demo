234763fec30807e56c1417039cc290ed554a84ac
wz2cool <wz2cool@live.cn>
2018-08-12 11:09:37 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,17 @@ filterUserIdGreaterThan500();
 
 function filterUserIdGreaterThan500() {
   console.log("total users count: ", users.length);
-  const query = new DynamicQuery<User>().addFilter({
-    propertyPath: "id",
-    operator: FilterOperator.GREATER_THAN,
-    value: 500
-  });
+  const query = new DynamicQuery<User>()
+    .addFilter({
+      propertyPath: "id",
+      operator: FilterOperator.GREATER_THAN,
+      value: 500
+    })
+    .addFilter({
+      propertyPath: "id",
+      operator: FilterOperator.LESS_THAN_OR_EQUAL,
+      value: 800
+    });
 
   const result = QueryProvider.query(users, query);
   console.log("after filter user count: ", result.length);

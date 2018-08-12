9c84ce47146155b4eab0db48918ddd18ec52f129
wz2cool <wz2cool@live.cn>
2018-08-12 11:31:58 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { values } from "./data";
 import { User } from "./models/user";
-import { DynamicQuery, FilterOperator, QueryProvider } from "ts-dynamic-query";
+import {
+  DynamicQuery,
+  FilterOperator,
+  QueryProvider,
+  FilterGroupDescriptor,
+  FilterCondition
+} from "ts-dynamic-query";
 
 const dataStr = JSON.stringify(values);
 const users = User.getUsersFromJSON(dataStr);
@@ -9,6 +15,7 @@ filterUserIdGreaterThan500();
 
 function filterUserIdGreaterThan500() {
   console.log("total users count: ", users.length);
+
   const query = new DynamicQuery<User>()
     .addFilter({
       propertyPath: "id",
@@ -19,8 +26,25 @@ function filterUserIdGreaterThan500() {
       propertyPath: "id",
       operator: FilterOperator.LESS_THAN_OR_EQUAL,
       value: 800
+    })
+    .addFilterGroup({
+      options: [
+        {
+          propertyPath: "firstName",
+          operator: FilterOperator.START_WITH,
+          value: "th",
+          ignoreCase: true
+        },
+        {
+          condition: FilterCondition.OR,
+          propertyPath: "firstName",
+          operator: FilterOperator.START_WITH,
+          value: "go",
+          ignoreCase: true
+        }
+      ]
     });
 
   const result = QueryProvider.query(users, query);
-  console.log("after filter user count: ", result.length);
+  console.log("after filter user count: ", JSON.stringify(result));
 }
